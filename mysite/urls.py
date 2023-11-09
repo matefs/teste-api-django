92c97486c5c60cb41b04653405c86a8299ed4912
Mateus Schverz <mate_fs@hotmail.com>
2023-11-09 20:39:49 -0300

    add: rota minha-rota

--- mysite/urls.py
+++ mysite/urls.py
@@ -17,6 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from . import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('minha-rota/', views.minha_view, name='minha_view'),
+
 ]
